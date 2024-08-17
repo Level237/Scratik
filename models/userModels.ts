0a1578c2811b7f89fe402a38ab7682e8e4d3478b
level237 <Bramslevel129@gmail.com>
2024-08-17 22:36:13 +0100

    model user

---
+++ models/userModels.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        require:[true,"Please provide a username"],
+        unique:true,
+    },
+    quarter:{
+        type:String,
+        require:[true,"Please provide a quarter"],
+    },
+    address:{
+        type:String,
+        require:[true,"Please provide a address"],
+    },
+    email:{
+        type:String,
+        require:[true,"Please provide a email"],
+        unique:true
+    },
+    phone:{
+        type:String,
+        require:[true,"Please provide a phone"],
+    },
+    login:{
+        type:String,
+        require:[true,"Please provide a login"],
+    },
+    password:{
+        type:String,
+        require:[true,"Please provide a password"]
+    },
+    role:{
+        type:String,
+        require:[true,"Please provide a role"]
+    },
+})
+
+const User=mongoose.models.users || mongoose.model('users',userSchema)
+
+export default User;
