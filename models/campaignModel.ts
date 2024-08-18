2ad3ce6a02d394e684d83c1c7da547823f9d99d0
level237 <Bramslevel129@gmail.com>
2024-08-18 23:53:55 +0100

    campaign model

---
+++ models/campaignModel.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+
+const CampaignSchema=new mongoose.Schema({
+
+
+    campaignName:{
+        type:String,
+        require:[true,'Please provide a campaign name']
+    },
+    campaignLots:{
+        type:String,
+        require:[true,"Please provide a campaign lots"]
+    },
+    campaignQuantity:{
+        type:String,
+        require:[true,"Please provide a campaign quantity"]
+    },
+    campaignBrand:{
+        type:String,
+        require:[true,"Please provide a campaign brand"]
+    },
+    winningPrice:{
+        type:Schema.Types.ObjectId,
+        ref:"Winning"
+    },
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    campaignTown:{
+        type:String,
+        require:[true,"Please provide a town"]
+    },
+    campaignPeriod:{
+        type:String,
+        require:[true,"Please provide a period"]
+    },
+    campaignDistrict:{
+        type:String,
+        require:[true,"Please provide a district"]
+    },
+    ticketNumber:{
+        type:Number,
+        require:[true,"Please provide a ticket"]
+    }
+})
