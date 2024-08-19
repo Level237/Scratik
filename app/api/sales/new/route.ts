20283213786b820ea2393dfd064dd9cd1c930f92
level237 <Bramslevel129@gmail.com>
2024-08-19 13:37:13 +0100

    new sale

---
+++ app/api/sales/new/route.ts
@@ -0,0 +1,46 @@
+import {connect} from "@/db/connect"
+import Sale from "@/models/pointSaleModel"
+import { NextRequest,NextResponse } from "next/server"
+
+
+
+
+connect()
+
+export async  function POST(request:NextRequest){
+
+    try {
+        const reqBody=await request.json();
+        const {quarter,nameSale,localisation,phoneSale,statutSale,typeSale,townSale,hours}=reqBody
+
+        console.log(reqBody)
+
+       
+
+        //Hash password
+        
+
+        const newSale=new Sale({
+            quarter,
+            nameSale,
+            localisation,
+            phoneSale,
+            statutSale,
+            typeSale,
+            townSale,
+            hours
+        })
+
+        const savedSale=await newSale.save()
+
+       
+
+        return NextResponse.json({message:"User created successfully",
+        success:true,
+        savedSale})
+
+
+    } catch (error:any) {
+        return NextResponse.json({error:error.message})
+    }
+}
