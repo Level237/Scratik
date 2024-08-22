9a156f4879de8805c70b26db257e504c3a46c974
level237 <Bramslevel129@gmail.com>
2024-08-22 13:19:54 +0100

    update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,29 +1,41 @@
+"use client"
 import React from 'react'
 import { Grip, LayoutGrid, LogOut, MapPin, Medal, Trophy, User } from 'lucide-react'
 import Image from 'next/image'
+import { SidebarStore } from '@/store/SidebarStore'
+import Link from 'next/link'
 export default function Sidebar() {
+
+    const currentPage=SidebarStore((set)=>set.currentPage)
+    const setCurrentPage=SidebarStore((set)=>set.setCurrentPage)
   return (
     <section style={{ background:"url('/bg.png')",backgroundPosition:"center",backgroundSize:"cover" }} 
     className='max-h-[33rem] px-5 py-8 sticky top-10 w-[12rem] bottom-24'>
 <Image className='w-[6rem]' width={80} height={80} src="/logo.png" alt=""/>
 
 <section className='flex flex-col mt-16 gap-6'>
-  <div className='flex cursor-pointer bg-[#57482959] py-2 px-4 rounded-sm items-center gap-2'>
+    <Link href="/dashboard" onClick={()=>setCurrentPage(1)}>
+    <div className={`flex cursor-pointer ${currentPage === 1 ? "bg-[#57482959] py-2 px-4 rounded-sm" : "" }   items-center gap-2`}>
       <div >
-      <LayoutGrid  className=' text-[#BE7E00]' />
+      <LayoutGrid  className={`${currentPage===1 ? "text-[#BE7E00]" : "text-white"}`} />
       </div>
       <div>
-          <h3 className='text-[#BE7E00] text-md'>Dashboard</h3>
+          <h3 className={`${currentPage===1 ? "text-[#BE7E00]" : "text-white"} text-md`}>Dashboard</h3>
       </div>
   </div>
-  <div className='flex cursor-pointer items-center gap-2'>
+    </Link>
+ 
+ <Link href="/users" onClick={()=>setCurrentPage(2)}>
+ <div className={`flex cursor-pointer ${currentPage === 2 ? "bg-[#57482959] py-2 px-4 rounded-sm" : "" } items-center gap-2`}>
       <div>
-      <User className='text-white' />
+      <User className={`${currentPage===2 ? "text-[#BE7E00]" : "text-white"}`} />
       </div>
       <div>
-          <h3 className='text-white text-md'>Utilisateurs</h3>
+          <h3 className={`${currentPage===2 ? "text-[#BE7E00]" : "text-white"}`}>Utilisateurs</h3>
       </div>
   </div>
+ </Link>
+ 
   <div className='flex cursor-pointer items-center gap-2'>
       <div>
       <MapPin className='text-white' />
