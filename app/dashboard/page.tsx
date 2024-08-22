a1067bc822a76acc5c88aa0e1306e54086de19fa
level237 <Bramslevel129@gmail.com>
2024-08-22 12:20:18 +0100

    layout dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,14 +5,11 @@ import React from 'react'
 
 export default function page() {
   return (
-    <section className='h-screen mx-12 my-16 flex justify-between'>
-      <Sidebar/>
-      <section className='flex flex-col gap-8'>
-            <div className='flex gap-3'>
-                <span className='text-gray-500'>Bienvenue</span>
-                <h3 className='text-white text-xl'>Maxime Eboue</h3>
-            </div>
-      </section>
-    </section>
+    <section className='flex flex-col gap-8'>
+    <div className='flex gap-3'>
+        <span className='text-gray-500'>Bienvenue</span>
+        <h3 className='text-white text-xl'>Maxime Eboue</h3>
+    </div>
+</section>
   )
 }
