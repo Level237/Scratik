61ff3b2f08db3fc5d5cf7ad1bc14a16e01f1c520
level237 <Bramslevel129@gmail.com>
2024-08-22 11:57:33 +0100

    update

---
+++ components/Sidebar.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Grip, LayoutGrid, MapPin, Medal, Trophy, User } from 'lucide-react'
+import Image from 'next/image'
+export default function Sidebar() {
+  return (
+    <section style={{ background:"url('/bg.png')",backgroundPosition:"center",backgroundSize:"cover" }} 
+    className='max-h-[33rem] px-5 py-8 sticky top-10 w-[12rem] bottom-24'>
+<Image className='w-[6rem]' width={80} height={80} src="/logo.png" alt=""/>
+
+<section className='flex flex-col mt-16 gap-6'>
+  <div className='flex cursor-pointer bg-[#57482959] py-2 px-4 rounded-sm items-center gap-2'>
+      <div >
+      <LayoutGrid  className=' text-[#BE7E00]' />
+      </div>
+      <div>
+          <h3 className='text-[#BE7E00] text-md'>Dashboard</h3>
+      </div>
+  </div>
+  <div className='flex items-center gap-2'>
+      <div>
+      <User className='text-white' />
+      </div>
+      <div>
+          <h3 className='text-white text-md'>Utilisateurs</h3>
+      </div>
+  </div>
+  <div className='flex items-center gap-2'>
+      <div>
+      <MapPin className='text-white' />
+      </div>
+      <div>
+          <h3 className='text-white text-md'>Point de ventes</h3>
+      </div>
+  </div>
+  <div className='flex items-center gap-2'>
+      <div>
+      <Trophy className='text-white' />
+      </div>
+      <div>
+          <h3 className='text-white text-md'>Lots à gagner</h3>
+      </div>
+  </div>
+  <div className='flex items-center gap-2'>
+      <div>
+      <Trophy className='text-white' />
+      </div>
+      <div>
+          <h3 className='text-white text-md'>Campaignes</h3>
+      </div>
+  </div>
+
+  <div className='flex items-center gap-2'>
+      <div>
+      <Medal className='text-white' />
+      </div>
+      <div>
+          <h3 className='text-white text-md'>Gagnants</h3>
+      </div>
+  </div>
+</section>
+    </section>
+  )
+}
