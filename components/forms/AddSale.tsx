7f50c2ceb9d20fe7aedad93742d78c268cbf2c12
level237 <Bramslevel129@gmail.com>
2024-08-28 12:41:37 +0100

    add sale init

--- components/forms/AddSale.tsx
+++ components/forms/AddSale.tsx
@@ -1,49 +1,116 @@
 'use client'
-import React from 'react'
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 export default function AddSale() {
+  const router=useRouter();
+  const [buttonDisable,setButtonDisabled]=React.useState(false)
+    const [loading,setLoading]=useState(false)
+  const [sale,setSale]=React.useState({
+    quarter:"",
+    nameSale:"",
+    localisation:"",
+    phoneSale:"",
+    statutSale:"",
+    typeSale:"",
+    townSale:"",
+    hours:""
+  })
+
+  const createSale=async ()=>{
+    try {
+        setLoading(true)
+        const response=await axios.post('/api/users/sales',sale)
+
+        console.log("signup success",response.data)
+        router.push("/users")
+    } catch (error:any) {
+        console.log("Signup failed",error.message)
+        //toast.error(error.message)
+    }finally{
+        setLoading(false)
+    }
+}
+
+useEffect(()=>{
+ 
+  
+},[])
   return (
     <section>
         <form action="">
         <div className='flex mb-5 items-center justify-between'>
             <div>
-                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Quartier' name="" id="" />
+                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Quartier' name="" id="" 
+                            value={sale.quarter}
+                            onChange={(e)=>setSale({...sale,quarter:e.target.value})}
+                            />
                 </div>
 
                 <div>
-                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder="Horaire d'ouverture" name="" id="" />
+                            <input type="time" className='bg-transparent py-3 px-3 border-b w-full' placeholder="Horaire d'ouverture" name="" id=""
+                             value={sale.hours}
+                             onChange={(e)=>setSale({...sale,hours:e.target.value})}
+                            />
                 </div>
       </div>
       <div className='flex mb-5 items-center justify-between'>
             <div>
-                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Nom du point de vente' name="" id="" />
+                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Nom du point de vente' name="" id=""
+                             value={sale.nameSale}
+                             onChange={(e)=>setSale({...sale,nameSale:e.target.value})}
+                            />
                 </div>
 
                 <div>
-                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Localisation exacte' name="" id="" />
+                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Localisation exacte' name="" id=""
+                             value={sale.localisation}
+                             onChange={(e)=>setSale({...sale,localisation:e.target.value})}
+                            />
                 </div>
       </div>
       <div className='flex mb-5 items-center justify-between'>
             <div>
-                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Numéro de téléphone' name="" id="" />
+                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Numéro de téléphone' name="" id="" 
+                             value={sale.quarter}
+                             onChange={(e)=>setSale({...sale,quarter:e.target.value})}
+                            />
                 </div>
 
                 <div>
-                            <input type="password" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Statut' name="" id="" />
+                  <select className='bg-transparent py-3 px-3 border-b w-52' name="" id=""
+                  onChange={(e)=>{
+                    
+                      setSale({...sale,statutSale:e.target.value})
+                  }}
+                  
+                  >
+                    <option value="">Statut</option>
+                    <option value="ouvert">Ouvert</option>
+                    <option value="fermé">Fermé</option>
+                  </select>
+                            
                 </div>
       </div>
 
       <div className='flex mb-5 items-center justify-between'>
             <div>
-                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Type de point de vente' name="" id="" />
+                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Type de point de vente' name="" id=""
+                            
+                            value={sale.typeSale}
+                            onChange={(e)=>setSale({...sale,typeSale:e.target.value})}/>
                 </div>
 
                 <div>
-                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Ville' name="" id="" />
+                            <input type="text" className='bg-transparent py-3 px-3 border-b w-full' placeholder='Ville' name="" id="" 
+                             value={sale.townSale}
+                             onChange={(e)=>setSale({...sale,townSale:e.target.value})}
+                            />
                 </div>
       </div>
       <div className='flex justify-end'>
-      <button  className='bg-[#BE7E00] px-6 py-3'>Ajouter</button>
+      <button  onClick={createSale} className='bg-[#BE7E00] px-6 py-3'>Ajouter</button>
       </div>
 
         </form>
