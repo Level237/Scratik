022bad705d37004ecc7fb49b608ef508d16dcaa8
level237 <Bramslevel129@gmail.com>
2024-09-08 12:36:02 +0100

    update

---
+++ app/api/users/me/route.ts
@@ -0,0 +1,24 @@
+import { getDataFromToken } from "@/helpers/getDataToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/db/connect";
+import SuperAdmin from "@/models/SuperAdmin";
+
+
+connect()
+
+export async function GET(request:NextRequest){
+    try {
+        const userId=await getDataFromToken(request);
+        const admin=await SuperAdmin.findOne({_id:userId}).select("-password")
+
+        return NextResponse.json(
+            {
+                message:"User found",
+                data:admin
+    })
+    } catch (error:any) {
+        console.log(error)
+        return NextResponse.json({error:error.message},{status:400})
+    }
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,31 @@
 
 import Sidebar from '@/components/Sidebar'
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ChartHotesse from '../../components/charts/chartHotesse';
 import ChartUser from '@/components/charts/chartUser';
 import ChartCampaign from '@/components/charts/chartCampaign';
+import axios from 'axios';
 
 export default function page() {
+
+  const [data,setData]=useState('')
+  const getUserDetails=async()=>{
+    const res=await axios.get('/api/users/me')
+    console.log(res.data)
+    setData(res.data.data.username)
+}
+useEffect(()=>{
+
+  getUserDetails()
+},[])
   return (
     <section className='flex flex-1 flex-col gap-8'>
     <div className='flex  justify-end gap-3'>
         <span className='text-gray-500'>Bienvenue</span>
-        <h3 className='text-white text-xl'>Maxime Eboue</h3>
+        <h3 className='text-white text-xl'>{data}</h3>
     </div>
     <section className='flex gap-8'>
       <ChartHotesse/>
